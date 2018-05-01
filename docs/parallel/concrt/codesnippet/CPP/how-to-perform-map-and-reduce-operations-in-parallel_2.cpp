736269d6a3ab877d19ccebb0a37b0792aecc5259
tuncatunc <atuncatunc@gmail.com>
2018-05-01 22:39:24 +0300

    Use uniform initializer where it's reasonable.

--- docs/parallel/concrt/codesnippet/CPP/how-to-perform-map-and-reduce-operations-in-parallel_2.cpp
+++ docs/parallel/concrt/codesnippet/CPP/how-to-perform-map-and-reduce-operations-in-parallel_2.cpp
@@ -1,22 +1,22 @@
     // File 1 
-    vector<wstring> v1;
-    v1.push_back(L"word1"); //1 
-    v1.push_back(L"word1"); //2 
-    v1.push_back(L"word2"); 
-    v1.push_back(L"word3"); 
-    v1.push_back(L"word4"); 
+    vector<wstring> v1 {
+      L"word1", // 1
+      L"word1", // 2
+      L"word2",
+      L"word3",
+      L"word4",
+    };
 
     // File 2 
-    vector<wstring> v2; 
-    v2.push_back(L"word5"); 
-    v2.push_back(L"word6"); 
-    v2.push_back(L"word7"); 
-    v2.push_back(L"word8"); 
-    v2.push_back(L"word1"); //3 
+    vector<wstring> v2 {
+      L"word5",
+      L"word6",
+      L"word7",
+      L"word8",
+      L"word1", // 3
+    }; 
 
-    vector<vector<wstring>> v;
-    v.push_back(v1);
-    v.push_back(v2);
+    vector<vector<wstring>> v { v1, v2 };
 
     concurrent_unordered_map<wstring, size_t> result;
     for_each(begin(v), end(v), [&result](const vector<wstring>& words) {
