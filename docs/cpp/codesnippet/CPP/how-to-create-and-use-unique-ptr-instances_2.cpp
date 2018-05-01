80a9c9258bf6d5991ee5c51a9ec2f4da5af012c3
tuncatunc <atuncatunc@gmail.com>
2018-05-01 23:14:38 +0300

    make_unique<Song> deletes the copy contructor of Song. Uniform initialization cannot be used at this example.

--- docs/cpp/codesnippet/CPP/how-to-create-and-use-unique-ptr-instances_2.cpp
+++ docs/cpp/codesnippet/CPP/how-to-create-and-use-unique-ptr-instances_2.cpp
@@ -1,13 +1,13 @@
 void SongVector()
 {
+    vector<unique_ptr<Song>> songs;
+    
     // Create a few new unique_ptr<Song> instances
     // and add them to vector using implicit move semantics.
-    vector<unique_ptr<Song>> songs {
-      make_unique<Song>(L"B'z", L"Juice"),
-      make_unique<Song>(L"Namie Amuro", L"Funky Town"),
-      make_unique<Song>(L"Kome Kome Club", L"Kimi ga Iru Dake de"),
-      make_unique<Song>(L"Ayumi Hamasaki", L"Poker Face")
-    };
+    songs.push_back(make_unique<Song>(L"B'z", L"Juice")); 
+    songs.push_back(make_unique<Song>(L"Namie Amuro", L"Funky Town")); 
+    songs.push_back(make_unique<Song>(L"Kome Kome Club", L"Kimi ga Iru Dake de")); 
+    songs.push_back(make_unique<Song>(L"Ayumi Hamasaki", L"Poker Face"));
 
     // Pass by const reference when possible to avoid copying.
     for (const auto& song : songs)
