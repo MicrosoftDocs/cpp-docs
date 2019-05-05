e689e7f4a37f3852c0d0ae808d8088210439be7a
Greg Degruy <gregory.degruy@gmail.com>
2019-05-05 10:17:33 -0700

    Added namespace and include

--- docs/cpp/codesnippet/CPP/how-to-create-and-use-shared-ptr-instances_3.cpp
+++ docs/cpp/codesnippet/CPP/how-to-create-and-use-shared-ptr-instances_3.cpp
@@ -1,16 +1,20 @@
-    vector<shared_ptr<Song>> v {
-      make_shared<Song>(L"Bob Dylan", L"The Times They Are A Changing"),
-      make_shared<Song>(L"Aretha Franklin", L"Bridge Over Troubled Water"),
-      make_shared<Song>(L"Thal�a", L"Entre El Mar y Una Estrella")
+#include <algorithm>
+#include <memory>
+#include <vector>
+
+    std::vector<std::shared_ptr<Song>> v {
+      std::make_shared<Song>(L"Bob Dylan", L"The Times They Are A Changing"),
+      std::make_shared<Song>(L"Aretha Franklin", L"Bridge Over Troubled Water"),
+      std::make_shared<Song>(L"Thal�a", L"Entre El Mar y Una Estrella")
     };
 
-    vector<shared_ptr<Song>> v2;
-    remove_copy_if(v.begin(), v.end(), back_inserter(v2), [] (shared_ptr<Song> s) 
+    std::vector<shared_ptr<Song>> v2;
+    std::remove_copy_if(v.begin(), v.end(), std::back_inserter(v2), [] (std::shared_ptr<Song> s) 
     {
         return s->artist.compare(L"Bob Dylan") == 0;		
     });
 
     for (const auto& s : v2)
     {
-        wcout << s->artist << L":" << s->title << endl;
+        std::wcout << s->artist << L":" << s->title << endl;
     }
