24f7515f08201e1e18c5611cbb8b6e993923e597
Greg Degruy <gregory.degruy@gmail.com>
2019-05-05 10:12:41 -0700

    Added namespace and include

--- docs/cpp/codesnippet/CPP/how-to-create-and-use-shared-ptr-instances_2.cpp
+++ docs/cpp/codesnippet/CPP/how-to-create-and-use-shared-ptr-instances_2.cpp
@@ -1,3 +1,5 @@
+#include <memory>
+    
     //Initialize with copy constructor. Increments ref count.
     auto sp3(sp2);
 
@@ -5,7 +7,7 @@
     auto sp4 = sp2;
 
     //Initialize with nullptr. sp7 is empty.
-    shared_ptr<Song> sp7(nullptr);
+    std::shared_ptr<Song> sp7(nullptr);
 
     // Initialize with another shared_ptr. sp1 and sp2
     // swap pointers as well as ref counts.
