419c920afee744848cdc6bf63de1e5b3d82b2b77
Schneider <lucas.schneider@sap.com>
2020-01-24 14:33:10 -0200

    catlservicemodulet-class_2.cpp: fix typo

--- docs/atl/reference/codesnippet/CPP/catlservicemodulet-class_2.cpp
+++ docs/atl/reference/codesnippet/CPP/catlservicemodulet-class_2.cpp
@@ -11,7 +11,7 @@ public:
       // your service
       // Suggested - PKT Level Authentication, 
       // Impersonation Level of RPC_C_IMP_LEVEL_IDENTIFY 
-      // and an appropiate Non NULL Security Descriptor.
+      // and an appropriate Non NULL Security Descriptor.
 
       return S_OK;
    }
