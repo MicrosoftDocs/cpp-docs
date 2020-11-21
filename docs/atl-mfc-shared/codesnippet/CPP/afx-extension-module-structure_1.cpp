9f76edcb8b05faf0c7e27992222e3aa07ef4194d
Yury Fedorov <yury.fedorov@gmail.com>
2020-11-21 01:29:52 +0100

    Update afx-extension-module-structure_1.cpp
    The first two parameters of the structure AFX_EXTENSION_MODULE are BOOL and HMODULE. So initialisation with = { NULL, NULL } is inaccurate: first NULL is FALSE (or 0 due to BOOL is defined as int). In fact this expression cannot be transformed to modern C++ equivalent = { nullptr, nullptr }.
    
    struct AFX_EXTENSION_MODULE
    {
    BOOL bInitialized;
    HMODULE hModule;
    HMODULE hResource;
    CRuntimeClass* pFirstSharedClass;
    COleObjectFactory* pFirstSharedFactory;
    };

--- docs/atl-mfc-shared/codesnippet/CPP/afx-extension-module-structure_1.cpp
+++ docs/atl-mfc-shared/codesnippet/CPP/afx-extension-module-structure_1.cpp
@@ -1,4 +1,4 @@
-static AFX_EXTENSION_MODULE NVC_MFC_DLLDLL = { NULL, NULL };
+static AFX_EXTENSION_MODULE NVC_MFC_DLLDLL = { 0 };
 extern "C" int APIENTRY
 DllMain(HINSTANCE hInstance, DWORD dwReason, LPVOID lpReserved)
 {
