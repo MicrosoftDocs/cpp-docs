28c56da88c35d35e15fbf4d542c98c346e18f034
Yury Fedorov <yury.fedorov@gmail.com>
2020-11-21 13:44:36 +0100

    Update afx-extension-module-structure_1.cpp
    We can keep it with default initialisation. Such case is defined in the section 3.5.7 of the C89 specification.

--- docs/atl-mfc-shared/codesnippet/CPP/afx-extension-module-structure_1.cpp
+++ docs/atl-mfc-shared/codesnippet/CPP/afx-extension-module-structure_1.cpp
@@ -1,4 +1,4 @@
-static AFX_EXTENSION_MODULE NVC_MFC_DLLDLL = { 0 };
+static AFX_EXTENSION_MODULE NVC_MFC_DLLDLL;
 extern "C" int APIENTRY
 DllMain(HINSTANCE hInstance, DWORD dwReason, LPVOID lpReserved)
 {
